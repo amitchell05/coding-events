0a7cf6f10479438be7d64f5e36a48927ece69224
Angela Mitchell <anglarenmit@gmail.com>
2022-07-25 23:57:33 -0500

    add filters to restrict access to pages based on login status

---
+++ src/main/java/org/launchcode/codingevents/AuthenticationFilter.java
@@ -0,0 +1,60 @@
+package org.launchcode.codingevents;
+
+import org.launchcode.codingevents.controllers.AuthenticationController;
+import org.launchcode.codingevents.data.UserRepository;
+import org.launchcode.codingevents.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class AuthenticationFilter extends HandlerInterceptorAdapter {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    AuthenticationController authenticationController;
+
+    private static final List<String> whitelist = Arrays.asList("/login", "/register", "/logout", "/css");
+
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response,
+                             Object handler) throws IOException {
+
+        // Don't require sign-in for whitelisted pages
+        if (isWhitelisted(request.getRequestURI())) {
+            // returning true indicates that the request may proceed
+            return true;
+        }
+
+        // Retrieves the user's session object (contained in the request
+        HttpSession session = request.getSession();
+        // Retrieves the User object corresponding to the given user
+        User user = authenticationController.getUserFromSession(session);
+
+        // The user is logged in => allows the request to be handled as normal
+        if (user != null) {
+            return true;
+        }
+
+        // The user is NOT logged in => redirect the user to the login page
+        response.sendRedirect("/login");
+        return false;
+    }
+
+    private static boolean isWhitelisted(String path) {
+        for (String pathRoot : whitelist) {
+            if (path.startsWith(pathRoot)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
